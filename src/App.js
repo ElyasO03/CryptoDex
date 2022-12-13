80929026c24638bbf9d52b77f52128dbc72998a2
ElyasO03 <elyas.ozbek07@gmail.com>
2022-12-13 13:11:08 -0500

    finish ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import Coins from "./components/Coins";
 import Navbar from "./components/Navbar";
@@ -7,15 +7,15 @@ import Coin from './routes/Coin'
 
 
 function App() {
-  
+
   const [coins, setCoins] = useState([])
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=99&page=1&sparkline=false'
+
 
   useEffect(() => {
-    axios.get(url).then((response) => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=9&page=1&sparkline=false').then((response) => {
       setCoins(response.data)
-      console.log(response.data)
+      //console.log(response.data)
     }).catch((error) => {
       console.log(error)
     })
@@ -23,12 +23,14 @@ function App() {
 
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element= {<Coins coins={coins}/>} />
-      <Route path="/coin" element={<Coin />} />
-        <Route path=":coinId" element={<Coin />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
+
     </>
   );
 }

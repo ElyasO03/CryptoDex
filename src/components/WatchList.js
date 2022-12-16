f0a6f748d7b55edebe2b0884af07f90e27fffa52
ElyasO03 <elyas.ozbek07@gmail.com>
2022-12-16 10:48:39 -0500

    last commit

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -3,15 +3,15 @@ import { connect } from 'react-redux'
 import './Coins.css'
 import { Link } from 'react-router-dom'
 import Coin from './Coin'
+import Button from '@mui/material/Button';
 
 
 const WatchList = (props) => {
 
-const handleDelete= (id) => {
-    console.log(id)
-    props.handleDelete(id)
-    // window.location.reload(false)
-} 
+    const handleDelete = (id) => {
+        console.log(id)
+        props.handleDelete(id)
+    }
 
     return (
         <div className='container'>
@@ -27,23 +27,24 @@ const handleDelete= (id) => {
 
                 {props.coins.map(coins => {
                     console.log(coins)
-                    console.log(coins.market_data.current_price.usd)
                     return (
                         <>
-                        <button onClick={()=> handleDelete(coins.id)}>Delete</button>
-                        <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
-                            <div className='coin-row'>
-                                <p>{coins.market_cap_rank}</p>
-                                <div className='img-symbol'>
-                                    <img src={coins.image.small} alt='' />
-                                    <p>{coins.symbol.toUpperCase()}</p>
-                                </div>
-                                <p>${coins.market_data.current_price.usd.toLocaleString()}</p>
-                                <p>{coins.market_data.price_change_percentage_24h.toFixed(2)}%</p>
-                                <p className='hide-mobile'>${coins.market_data.total_volume.usd.toLocaleString()}</p>
-                                <p className='hide-mobile'>${coins.market_data.market_cap.usd.toLocaleString()}</p>
+                            <div className='btnD'>
+                                <Button onClick={() => handleDelete(coins.id)} variant="outlined" color="error">Delete {coins.name}</Button>
                             </div>
-                        </Link>
+                            <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
+                                <div className='coin-row'>
+                                    <p>{coins.market_cap_rank}</p>
+                                    <div className='img-symbol'>
+                                        <img src={coins.image.small} alt='' />
+                                        <p>{coins.symbol.toUpperCase()}</p>
+                                    </div>
+                                    <p>${coins.market_data.current_price.usd.toLocaleString()}</p>
+                                    <p>{coins.market_data.price_change_percentage_24h.toFixed(2)}%</p>
+                                    <p className='hide-mobile'>${coins.market_data.total_volume.usd.toLocaleString()}</p>
+                                    <p className='hide-mobile'>${coins.market_data.market_cap.usd.toLocaleString()}</p>
+                                </div>
+                            </Link>
                         </>
                     )
                 })}
@@ -61,7 +62,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        handleDelete: (id) => dispatch({type: 'DELETE_COIN', payload: id})
+        handleDelete: (id) => dispatch({ type: 'DELETE_COIN', payload: id })
     }
 }
 
